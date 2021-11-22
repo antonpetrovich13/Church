5e507af187ec6256127729e30aac44a07d59686e
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-11-22 20:08:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -28,3 +28,16 @@ if ((document.documentElement.offsetWidth < 900) && (document.documentElement.of
 if (document.documentElement.scrollTop > 100) {
 	head.classList.add('_active')
 }*/
+
+
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
